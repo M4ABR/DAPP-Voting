3dbfa833eda5fe303eaaac02d9362e82e17a2833
ARslan Ahmad <mycomputer2214@gmail.com>
2025-05-27 09:42:24 +0500

    [docs] Added Readme, for intial setup

--- context/constants.js
+++ context/constants.js
@@ -2,6 +2,6 @@
 //Here is the address to which you wnat to make a transaction the above one is example
 import voting from './Create.json'
 
-export const VotingAddress='0xeA5f798e20357214A75400D963bcd2b303e6B586';
+export const VotingAddress='0x4F8Ca26CE062fCD83C0d3b462008042E4Cd25b0D';
 export const VotingAddressABI=voting.abi;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/naB7EVkL9rslZ7MxppLANOFVPQw3sDu2", // Infura endpoint
       accounts: [
-        "bf6241a0679e93330484de07412bb05832fdcd56d20017305332639425ebb652" // Replace with your private key
+        "3f87af3e5e23e592417e3b48d3572157c48db35e1ef853e9c93de54042061947" // Replace with your private key
       ],
       chainId: 11155111 // Add this line
     }
